c7307b80511059f03a0ada3d092ecb828349c15f
Felipe Guedes <felipecg2k2@gmail.com>
2025-10-08 22:16:25 -0300

    Alterações 08/10/25

---
+++ cap03/ex03-4.py
@@ -0,0 +1,5 @@
+salario = 1200
+
+pagaImposto = salario >=1200
+
+print(pagaImposto)
---
+++ cap03/ex03-6.py
@@ -0,0 +1,9 @@
+media = 7
+
+materia1 = 6
+materia2 = 7
+materia3 = 8
+
+aprovado = materia1 >= media and materia2 >= media and materia3 >= media
+
+print(aprovado)
